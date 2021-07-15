1fcdd778312be8190820cba61e1d4f0270b67e46
seungjohan <cmostrillion@gmail.com>
2021-07-15 14:01:08 +0900

    initilize variables

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
 //alert('Im Seungjo. Im student.');
-console.log('Im Seungjo.Im student');
+//console.log('Im Seungjo.Im student');
+
+//variable, calculate
+//let a = 221;
+const a = 221; // It can't be changed when a is a constant
+let b = a - 5;
+a = 4; //replaced from 221 to 4
+console.log(b, a);
