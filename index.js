d7ff5ce47ab6f12bf073589d676485010ec2192f
seungjohan <cmostrillion@gmail.com>
2021-07-15 17:28:28 +0900

    DOM if-else

--- index.js
+++ index.js
@@ -120,12 +120,101 @@ function handleResize(event){
     console.log(event);
     //console.log("I have been resized");
 }
-
+/*
 function handleClick() {
     title.style.color = "blue";
 }
-
+*/
 //window.addEventListener("resize",handleResize(event));
 title.addEventListener("click", handleClick);
 
 
+//if-else
+if(10 > 5) { // === 두 변수가 같은지 확인
+    //if ("seungjo" === "seungmoke")
+    console.log("hi");
+    // else if {}
+} else {
+    console.log("ho");
+}
+
+if (20 > 5 && "seungjo" === "seungjo") { // and
+//if (20 > 5 || "seungjo" === "seungjo") { // or
+    console.log("yes");
+} else {
+    console.log("no");
+}
+
+
+const age = prompt("What's ur name?") //ask sth to user
+//console.log(age);
+
+if(age > 18) {
+    console.log("You can drink");
+} else {
+    console.log("You can't");
+}
+
+
+const BASE_COLOR = "red";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+    const currentColor = title.style.color;
+
+    if(currentColor === BASE_COLOR) {
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
+}
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("mouseenter", handleClick);
+}
+init();
+
+
+//example about event
+function handleOffline() {
+    console.log("Bye Bye");
+}
+
+function handleOnline() {
+    console.log("Welcome back");
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
+
+
+//DOM if-else function
+const title = document.querySelector("title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClicked() {
+    //const currentClass = title.className;
+    const hasClass = title.classList.contains((CLICKED_CLASS));
+
+    //if(currentClass !== CLICKED_CLASS){
+    if (!hasClass) {
+        //title.className = CLICKED_CLASS;
+        title.classList.add(CLICKED_CLASS);
+    } else {
+        //title.className = "";
+        title.classList.remove(CLICKED_CLASS);
+    }
+}
+//위의 함수와 동일한 event
+function handleClicked2(){
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+
+
+function init() {
+    title.addEventListener("click", handleClick);
+}
+init();
